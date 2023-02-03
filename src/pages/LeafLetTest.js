c0358c45b5060dcf97400d9695ccb137e7465673
JAUBERTIE <arnaud.jaubertie@capgemini.com>
2023-02-03 15:38:31 +0100

    Marker

--- src/pages/LeafLetTest.js
+++ src/pages/LeafLetTest.js
@@ -16,6 +16,11 @@ export default class LeafLetTest extends Component {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
           </MapContainer>
           </div>
         )
